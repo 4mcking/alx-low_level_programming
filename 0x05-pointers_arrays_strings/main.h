b299f055301ba273a087b9901005c80dff77414b
4mcking <mckingchi@gmail.com>
2022-06-13 21:16:56 +0000

    header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,6 +13,7 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+size_t _strlen(const char *str);
 int _strlen(const char *s);
 void _puts(char *str);
 void print_rev(char *s);
