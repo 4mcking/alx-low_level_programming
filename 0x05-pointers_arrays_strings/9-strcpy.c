7d8680e2f3e51e7bbe09f7168f34f3a713800601
4mcking <mckingchi@gmail.com>
2022-06-13 22:36:22 +0000

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	while (src[index] != '\0')
 	{
 		dest[index] = src[index];
 		index++;
 	}
-
+	dest[index] = '\0';
 	return (dest);
 }
