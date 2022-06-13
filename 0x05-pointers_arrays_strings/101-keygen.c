7c996afd2451902e7f8a00a86d73ff5e84d74ce4
4mcking <mckingchi@gmail.com>
2022-06-13 16:59:11 +0000

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -20,16 +20,13 @@ int main(void)
 		password[index] = 33 + rand() % 94;
 		sum += password[index++];
 	}
-
 	password[index] = '\0';
-
 	if (sum != 2772)
 	{
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
-
 		for (index = 0; password[index]; index++)
 		{
 			if (password[index] >= (33 + diff_half1))
@@ -46,8 +43,6 @@ int main(void)
 				}
 			}
 		}
-
 		printf("%s", password);
-
 		return (0);
 }
