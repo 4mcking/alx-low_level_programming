f075a8ce437672d3eb3a69208a5eb4e97de90b03
4mcking <mckingchi@gmail.com>
2022-06-20 11:39:23 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - return length of string that matches values consistently
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				result++;
+				break;
+			}
+		}
+		if (s[j] == '\0')
+			return (result);
+	}
+	return (result);
+}
