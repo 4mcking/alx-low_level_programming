f4151a4f0718b7f443b97bbc90d611ac720c98d5
4mcking <mckingchi@gmail.com>
2022-06-22 11:06:13 +0000

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - print string with new line
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+		_putchar('\n');
+}
