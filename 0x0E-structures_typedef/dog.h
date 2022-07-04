f4cc6ea169f4ccde31797009c261cc4b1bc07181
4mcking <mckingchi@gmail.com>
2022-07-04 19:30:40 +0000

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,11 +7,11 @@
  * Desc: Header file that defines a new type struct dog.
  */
 
-/*
- * struct dog - new type describing dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
@@ -20,12 +20,11 @@ struct dog
 	char *owner;
 };
 
-/*
- * dog_t - Typedef for struct dog
- */
+/**
+ *  * dog_t - Typedef for struct dog
+ *   */
 typedef struct dog dog_t;
 
-int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
