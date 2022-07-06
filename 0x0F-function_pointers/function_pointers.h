c34a95b915022526b56a1159a5529bf98fd4dd6f
4mcking <mckingchi@gmail.com>
2022-07-06 20:13:09 +0000

    header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
