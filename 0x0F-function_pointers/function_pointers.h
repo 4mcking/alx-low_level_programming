650057118ec325ef2b0f8dfcc00ba1ee26460e60
4mcking <mckingchi@gmail.com>
2022-07-06 12:18:07 +0000

    header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -8,6 +8,9 @@
  *       used in the 0x0F-function_pointers directory.
  */
 
+#include <stddef.h>
+#include <stdlib.h>
+
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
