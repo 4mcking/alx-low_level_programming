fe8ff3fd77c44f55971a51c4e470d3faa50c36af
4mcking <mckingchi@gmail.com>
2022-07-06 11:57:30 +0000

    header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,16 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+/**
+ * File: function_pointers.h
+ * Auth: King CEO
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0F-function_pointers directory.
+ */
+
+int _putchar(char);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
