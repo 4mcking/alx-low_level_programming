c30d2c69af08ae0dbee9fbec7065f3d3d6a3474c
4mcking <mckingchi@gmail.com>
2022-07-07 03:37:57 +0000

    What's my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * File: function_pointers.h
@@ -8,15 +11,8 @@
  *       used in the 0x0F-function_pointers directory.
  */
 
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
-
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-void print_opcode(char *op, int n);
 
 #endif
