f583859ed8e5dbfede6137796c4271fb9d80f996
McKing <97905873+4mcking@users.noreply.github.com>
2022-07-07 15:27:55 +0100

    One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,40 +9,25 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list params;
+	va_list ap;
+	unsigned int index;
+	char *str;
 
-	va_start(params, n);
-	if (n != 0)
+	/* iterate the argument list from the start */
+	va_start(ap, n);
+
+	for (index = 0; index < n; index++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (i != n - 1)
-			{
-				if (separator != NULL)
-					printf("%s%s", make_nil(va_arg(params, char *)), separator);
-				else
-					printf("%s", make_nil(va_arg(params, char *)));
-			}
-			else
-			{
-				printf("%s", make_nil(va_arg(params, char *)));
-			}
-		}
-	}
-	printf("\n");
-	va_end(params);
-}
+		str = va_arg(ap, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
 
-/**
- * make_nil - change s to be "(nil)" if s is null
- * @s: string to be used
- *
- * Return: pointer to @s
- */
-char *make_nil(char *s)
-{
-	if (s == NULL)
-		s = "(nil)";
-	return (s);
+		if (separator && index != n - 1)
+			printf("%s", separator);
+	}
+	/*clean up*/
+	va_end(ap);
+	printf("\n");
 }
