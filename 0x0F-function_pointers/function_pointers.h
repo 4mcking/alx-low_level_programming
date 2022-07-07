2297bc5bda8e825ec4e1ed7201500b2dde861a3a
4mcking <mckingchi@gmail.com>
2022-07-07 03:41:12 +0000

    header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,15 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * File: function_pointers.h
- * Auth: King CEO
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0F-function_pointers directory.
- */
-
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
+/**
+ * enum bool - typedef function for bool
+ * @TRUE: 1
+ * @FALSE: 0
+ */
+typedef enum bool
+{
+	TRUE = 1,
+	FALSE = 0
+} bool;
 
 #endif
