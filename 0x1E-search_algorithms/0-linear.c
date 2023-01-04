9b07ecd22995a872505c0c7ba7fdcb669b1dd758
4mcking <mckingchi@gmail.com>
2023-01-04 21:46:54 +0000

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,19 +12,17 @@
   *
   * Description: Prints a value every time it is compared in the array.
   */
-int linear_search(int *array, size_t size, int value)
-{
-	size_t i;
+int linear_search(int *array, size_t size, int value) {
+    if (array == NULL) {
+        return -1;
+    }
 
-	if (array == NULL)
-		return (-1);
+    for (size_t i = 0; i < size; i++) {
+        printf("%d\n", array[i]);
+        if (array[i] == value) {
+            return i;
+        }
+    }
 
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
-
-	return (-1);
+    return -1;
 }
